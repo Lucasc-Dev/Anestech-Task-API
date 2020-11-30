8ea9f7feab5bb93e69e893904c0609a63cbff0a2
Lucas César <lucasambrogi1@hotmail.com>
2020-11-29 23:52:36 -0300

    Users route and its controller: added update, delete and show user route.

--- src/modules/users/infra/http/controllers/UsersController.js
+++ src/modules/users/infra/http/controllers/UsersController.js
@@ -1,17 +1,32 @@
 const UsersRepository = require('../../sequelize/repositories/UsersRepository');
+const RolesRepository = require('../../sequelize/repositories/RolesRepository');
 
 const CreateUserService = require('../../../services/CreateUserService');
+const ShowUserService = require('../../../services/ShowUserService');
 const ListUsersService = require('../../../services/ListUsersService');
+const DeleteUserService = require('../../../services/DeleteUserService');
+const UpdateUserService = require('../../../services/UpdateUserService');
 
 const usersRepository = new UsersRepository();
+const rolesRepository = new RolesRepository();
 
 class UsersController {
     async create(request, response) {
-        const { name, email } = request.body;
+        const { name, email, roles } = request.body;
 
-        const createUser = new CreateUserService(usersRepository);
+        const createUser = new CreateUserService(usersRepository, rolesRepository);
 
-        const user = await createUser.execute({ name, email });
+        const user = await createUser.execute({ name, email, roles });
+
+        return response.json(user);
+    }
+
+    async show(request, response) {
+        const { user_id } = request.params;
+
+        const showUser = new ShowUserService(usersRepository);
+
+        const user = await showUser.execute(user_id);
 
         return response.json(user);
     }
@@ -24,6 +39,27 @@ class UsersController {
         const users = await listUsers.execute({ page });
 
         return response.json(users);
+    }
+
+    async update(request, response) {
+        const { user_id } = request.params;
+        const { email, name } = request.body;
+
+        const updateUser = new UpdateUserService(usersRepository);
+
+        const user = await updateUser.execute({ user_id, email, name });
+
+        return response.json(user);
+    }
+
+    async delete(request, response) {
+        const { user_id } = request.params;
+
+        const deleteUser = new DeleteUserService(usersRepository);
+
+        await deleteUser.execute(user_id);
+
+        return response.status(200).send();
     }
 }
 
--- src/modules/users/infra/http/routes/users.routes.js
+++ src/modules/users/infra/http/routes/users.routes.js
@@ -7,5 +7,9 @@ const router = Router();
 const usersController = new UsersController();
 
 router.post('', usersController.create);
+router.get('/:user_id', usersController.show);
+router.get('', usersController.index);
+router.put('/:user_id', usersController.update);
+router.delete('/:user_id', usersController.delete);
 
 module.exports = router;
