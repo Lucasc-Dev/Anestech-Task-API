9dd03349b436824d20489f018d9a30445354a05b
Lucas César <lucasambrogi1@hotmail.com>
2020-11-29 23:50:13 -0300

    DeleteUser service: created.

---
+++ src/modules/users/services/DeleteUserService.js
@@ -0,0 +1,21 @@
+const AppError = require('../../../shared/errors/AppError');
+
+class DeleteUserService {
+    constructor(usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    async execute(user_id) {
+        const user = await this.usersRepository.findById(user_id);
+
+        if (!user) {
+            throw new AppError('User not found');
+        }
+
+        await this.usersRepository.delete(User);
+
+        return;
+    }
+}  
+
+module.exports = DeleteUserService;
